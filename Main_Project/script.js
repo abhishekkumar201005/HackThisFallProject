19a5e2e2b4dedc8529aa86d0bacdbfe6f4891c65
Abhishek-Kumar <abhishekrana201005@gmail.com>
2021-10-24 03:14:39 +0530

    full responsive ----only wait for events and engage page

--- Main_Project/script.js
+++ Main_Project/script.js
@@ -1,27 +1,11 @@
+// Make mobile navigation work
 
-///////////////////////////////////////////////////////////
-// Smooth scrolling animation
-// const allLinks = document.querySelectorAll("a:link");
-// allLinks.forEach(function (link) {
-//   link.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const href = link.getAttribute("href");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-//     // Scroll back to top
-//     if (href === "#")
-//       window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//       });
-
-//     // Scroll to other links
-//     if (href !== "#" && href.startsWith("#")) {
-//       const sectionEl = document.querySelector(href);
-//       sectionEl.scrollIntoView({ behavior: "smooth" });
-//     }
-//   });
-// });
-
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 ///////////////////////////////////////////////////////////
 // Sticky navigation
 
