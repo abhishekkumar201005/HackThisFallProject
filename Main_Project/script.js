3848f36f71fbf479031177a385fdeeb2c0390c3f
Abhishek-Kumar <abhishekrana201005@gmail.com>
2021-10-24 07:32:49 +0530

    wait for aditya work

--- Main_Project/script.js
+++ Main_Project/script.js
@@ -10,7 +10,6 @@ btnNavEl.addEventListener("click", function () {
 // Sticky navigation
 
 const sectionHeroEl = document.querySelector(".section-hero");
-
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
@@ -28,7 +27,7 @@ const obs = new IntersectionObserver(
     // In the viewport
     root: null,
     threshold: 0,
-    rootMargin: "-90px",
+    rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroEl);
@@ -49,4 +48,34 @@ function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
     })
+}
+//feedback
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
+
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
+})
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank You!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <h5>We'll use your feedback to improve our customer support</h5>
+    `
+})
+
+function removeActive() {
+    for(let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
 }
